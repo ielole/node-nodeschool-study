a294fb434baaacf5b1592b0810bdd524ced739d6
ielole <ieloletin@gmail.com>
2016-12-11 22:13:18 -0500

    Complete baby-steps.
    Looked up solution after multiple attempts returning 0.
    Was expecting to write a function and have process.argv passed in.

--- learnyounode/01-baby-steps.js
+++ learnyounode/01-baby-steps.js
@@ -1,11 +1,10 @@
 'use strict';
 
 let sum = 0;
-const getSum = function(array) {
-for (let i = 2; i < array.length; i++) {
-  sum = sum + array[i];
-}
-return sum;
-};
 
-console.log(process.argv, getSum(process.argv));
+for (let i = 2; i < process.argv.length; i++) {
+    sum += Number(process.argv[i]);
+  }
+
+
+console.log(sum);
